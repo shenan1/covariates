7533806bd220aa6cfbecf97ac5688c5b65e5f851
shenan1 <s.grossberg@btinternet.com>
2020-07-20 14:47:28 +0100

    minor changes

--- covariates.R
+++ covariates.R
@@ -1,3 +1,5 @@
+# https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
+
 install.packages("devtools")
 library("devtools")
 library("roxygen2")
@@ -5,3 +7,9 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 document()
 setwd("..")
 install("covariates")
+
+# install from github:
+install_github('shenan1/covariates')
+
+# prefix functions with namespace covariates:::
+# update library
