b8cc9bb24e5d961196d68184d76452ed9483dff2
shenan1 <s.grossberg@btinternet.com>
2020-12-25 20:03:08 +0000

    updated loadData functions to return origin from which time is counted

--- R/loadData2d.R
+++ R/loadData2d.R
@@ -1,22 +1,27 @@
 #' Load covariate data, including 2D, time and 1 variable
 #' Includes flag for data source
+#' Also returns origin from which time is counted
 #' 
 #' @param file path to file
 #' @param source ECMWF
 #' 
-#' @return Covariate data.
+#' @return Covariate data and origin.
 load.data.2d <- function(file, source = 'CMEMS') {
   if (source == 'ECMWF') {
     lon.id <- 'longitude'
-    lat.id <- 'latitude'} 
-  else {
-    lon.id <- 'lon'
-    lat.id <- 'lat'}
+    lat.id <- 'latitude'} else {
+      lon.id <- 'lon'
+      lat.id <- 'lat'}
   covariate <- nc_open(file)
   lon <- ncvar_get(covariate, varid = lon.id)
   lat <- ncvar_get(covariate, varid = lat.id)
   time <- ncvar_get(covariate, varid = 'time')
   var <- ncvar_get(covariate)
+  
+  text1 <- grep("since", as.character(covariate), value=TRUE)[1] # returns first line containing "since"
+  text2 <- sub(".*since ", "", text1) # returns everything after "since " in text1
+  origin <- str_sub(text2, 1, 10) # returns characters 1-10 in text2
+  
   nc_close(covariate)
   rm(covariate)
   if (source == 'ECMWF') {
@@ -25,4 +30,5 @@ load.data.2d <- function(file, source = 'CMEMS') {
   return(list(lon = lon, 
               lat = lat, 
               time = time, 
-              var = var))}
+              var = var, 
+              origin = origin))}
--- R/loadData3d.R
+++ R/loadData3d.R
@@ -1,46 +1,56 @@
 #' Load covariate data, including 3D, time and 1 variable
 #' Includes flags for variable and data source
+#' Also returns origin from which time is counted (excludes MASS data)
 #' 
 #' @param file path to file
 #' @param variable uo, vo, chlf, chlr, oxyf, oxyr
 #' @param source MASS
 #' 
-#' @return Covariate data.
+#' @return Covariate data and origin.
 load.data.3d <- function(file, variable = 'thetao', source = 'CMEMS') {
   if (source == 'MASS') {
     lon.id <- 'longitudes'
     lat.id <- 'latitutdes'
-    depth.id <- 'depths'} 
-  else {
-    lon.id <- 'lon'
-    lat.id <- 'lat'
-    depth.id <- 'depth'} 
+    depth.id <- 'depths'} else {
+      lon.id <- 'lon'
+      lat.id <- 'lat'
+      depth.id <- 'depth'}
   covariate <- nc_open(file)
   lon <- ncvar_get(covariate, varid = lon.id)
   lat <- ncvar_get(covariate, varid = lat.id)
   depth <- ncvar_get(covariate, varid = depth.id)
+  
+  # var <- case_when(
+  #   variable == 'uo' ~ ncvar_get(covariate, varid = 'uo'),
+  #   variable == 'vo' ~ ncvar_get(covariate, varid = 'vo'),
+  #   variable == 'chlf' ~ ncvar_get(covariate, varid = 'ChlTot_fore'),
+  #   variable == 'chlr' ~ ncvar_get(covariate, varid = 'ChlTot_rean'),
+  #   variable == 'oxyf' ~ ncvar_get(covariate, varid = 'Ox_fore'),
+  #   variable == 'oxyr' ~ ncvar_get(covariate, varid = 'Oxy_rean'),
+  #   variable == 'thetao' ~ ncvar_get(covariate))
+  
   if (variable == 'uo') {
-    var <- ncvar_get(covariate, varid = 'uo')} 
-  else if (variable == 'vo') {
-    var <- ncvar_get(covariate, varid = 'vo')} 
-  else if (variable == 'chlf') {
-    var <- ncvar_get(covariate, varid = 'ChlTot_fore')}
-  else if (variable == 'chlr') {
-    var <- ncvar_get(covariate, varid = 'ChlTot_rean')}
-  else if (variable == 'oxyf') {
-    var <- ncvar_get(covariate, varid = 'Ox_fore')}
-  else if (variable == 'oxyr') {
-    var <- ncvar_get(covariate, varid = 'Oxy_rean')} 
-  else {
-    var <- ncvar_get(covariate)}
+    var <- ncvar_get(covariate, varid = 'uo')} else if (variable == 'vo') {
+      var <- ncvar_get(covariate, varid = 'vo')} else if (variable == 'chlf') {
+        var <- ncvar_get(covariate, varid = 'ChlTot_fore')} else if (variable == 'chlr') {
+          var <- ncvar_get(covariate, varid = 'ChlTot_rean')} else if (variable == 'oxyf') {
+            var <- ncvar_get(covariate, varid = 'Ox_fore')} else if (variable == 'oxyr') {
+              var <- ncvar_get(covariate, varid = 'Oxy_rean')} else {
+                var <- ncvar_get(covariate)}
+  
   if (source == 'MASS') {
-    time <- (1:length(var[1,1,1,]))*3600*24 - 1800*24}
-  else {
-    time <- ncvar_get(covariate, varid = 'time')}
+    time <- (1:length(var[1,1,1,]))*3600*24 - 1800*24
+    origin <- NULL} else {
+      time <- ncvar_get(covariate, varid = 'time')
+      text1 <- grep("since", as.character(covariate), value=TRUE)[1] # returns first line containing "since"
+      text2 <- sub(".*since ", "", text1) # returns everything after "since " in text1
+      origin <- str_sub(text2, 1, 10)} # returns characters 1-10 in text2
+  
   nc_close(covariate)
   rm(covariate)
   return(list(lon = lon, 
               lat = lat, 
               depth = depth, 
               time = time, 
-              var = var))}
+              var = var, 
+              origin = origin))}
