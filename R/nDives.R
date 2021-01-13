f7212841bab26733e1edbff2a0536bb8eea2efb3
Shenan Grossberg <shenangrossberg@Shenans-MacBook-Air.local>
2021-01-13 14:36:49 +0000

    changed default dive.time in nDives.R to 3600

--- R/nDives.R
+++ R/nDives.R
@@ -7,7 +7,7 @@
 #' @param comms communication time with glider between flights [minutes]
 #' 
 #' @return Number of dives.
-n.dives <- function(gldr.time.end, pilot.start = 7, pilot.end = 22, dives.flight = 3, seconds.dive = 3832, comms = 20) {
+n.dives <- function(gldr.time.end, pilot.start = 7, pilot.end = 22, dives.flight = 3, seconds.dive = 3600, comms = 20) {
 gldr.time.end.mod <- gldr.time.end %% (24*3600)
 last.dive <- pilot.end*3600 - dives.flight*seconds.dive - comms*60
 first.dive <- (pilot.start + 24)*3600
