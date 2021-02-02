20623c968f1afaff14591d8f40b9ae64c5c8701f
Shenan Grossberg <shenangrossberg@Shenans-MacBook-Air.local>
2021-02-02 20:38:15 +0000

    added print statement to inlaError.R

--- R/inlaError.R
+++ R/inlaError.R
@@ -2,13 +2,14 @@
 #' 
 #' @param mod.mode result of inla
 #' 
-#' @return 0 = no crash; 1 = crash.
+#' @return 0 = no crash; !0 = crash.
 inla.error <- function(mod.mode, l = 1, k, RData) {
   
   load(paste(RData, 'errors.RData', sep = ''))
   
   if (class(mod.mode) != "inla") {
     errors[l, k] <- errors[l, k] + 1
+    print("INLA error; repeating calculation...")
     save(errors, file = paste(RData, 'errors.RData', sep = ''))} else {
       errors[l, k] <- 0}
   
