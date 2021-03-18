d4e529d99e21c3905c0f80fadd45b46fd6623727
Shenan Grossberg <s.grossberg@btinternet.com>
2021-03-18 16:49:54 +0000

    modified inlaSummary.R to handle extra hyperparameter

--- R/inlaSummary.R
+++ R/inlaSummary.R
@@ -7,12 +7,13 @@
 #' @param write write the results (TRUE or FALSE)
 #' 
 #' @return Fixed effects mean and sd; range 0.025 and stdev 0.975 quantiles.
-inla.summary <- function(mod.mode, n.fixed, l = 1, k, RData, directory, write = FALSE) {
+inla.summary <- function(mod.mode, n.fixed, n.hyper, l = 1, k, RData, directory, write = FALSE) {
   
   load(paste(RData, 'beta.mean.RData', sep = ''))
   load(paste(RData, 'beta.sd.RData', sep = ''))
   load(paste(RData, 'p.range.RData', sep = ''))
   load(paste(RData, 'stdev.RData', sep = ''))
+  load(paste(RData, 'rho.RData', sep = ''))
   
   summary.char <- as.character(summary(mod.mode))
   fixed.effects <- summary.char[3]
@@ -23,29 +24,33 @@ inla.summary <- function(mod.mode, n.fixed, l = 1, k, RData, directory, write = 
       beta.sd[[i]][l, k]   <- strsplit(fixed.effects, ", ")[[1]][n.fixed + i]}
     beta.mean[[1]][l, k] <- 
       substr(beta.mean[[1]][l, k], 3, nchar(beta.mean[[1]][l, k]))              # remove first 2 characters
-    p.range[l, k] <- strsplit(hyperparameters, ", ")[[1]][8]                    # extract practical range 0.025 quantile
-    stdev[l, k]   <- strsplit(hyperparameters, ", ")[[1]][15]                   # extract stdev 0.975 quantile
-    stdev[l, k] <- substr(stdev[l, k], 1, nchar(stdev[l, k]) - 1)} else {       # remove last character
+    p.range[l, k] <- strsplit(fixed.effects, ", ")[[1]][2*n.hyper + 2]          # extract practical range 0.025 quantile
+    stdev[l, k]   <- strsplit(fixed.effects, ", ")[[1]][4*n.hyper + 3]          # extract stdev 0.975 quantile
+    rho[l, k]     <- strsplit(fixed.effects, ", ")[[1]][3*n.hyper]                # extract rho 0.025 quantile
+    rho[l, k]     <- substr(rho[l, k], 1, nchar(rho[l, k]) - 1)} else {           # remove last character
       for (i in 1:n.fixed) {
         beta.mean[[i]][l, k] <- beta.sd[[i]][l, k] <- NA}
-      p.range[l, k] <- strsplit(fixed.effects, ", ")[[1]][8]
-      stdev[l, k]   <- strsplit(fixed.effects, ", ")[[1]][15]
-      stdev[l, k]   <- substr(stdev[l, k], 1, nchar(stdev[l, k]) - 1)}
+      p.range[l, k] <- strsplit(fixed.effects, ", ")[[1]][2*n.hyper + 2]
+      stdev[l, k]   <- strsplit(fixed.effects, ", ")[[1]][4*n.hyper + 3]
+      rho[l, k]     <- strsplit(fixed.effects, ", ")[[1]][3*n.hyper]
+      rho[l, k]     <- substr(rho[l, k], 1, nchar(rho[l, k]) - 1)}
   
   for (i in 1:n.fixed) {
     beta.mean[[i]] <- apply(beta.mean[[i]], 1:2, as.numeric)
     beta.sd[[i]]   <- apply(beta.sd[[i]], 1:2, as.numeric)}
   p.range <- apply(p.range, 1:2, as.numeric)
   stdev   <- apply(stdev, 1:2, as.numeric)
+  rho     <- apply(rho, 1:2, as.numeric)
   
   save(beta.mean, file = paste(RData, 'beta.mean.RData', sep = ''))
   save(beta.sd, file = paste(RData, 'beta.sd.RData', sep = ''))
   save(p.range, file = paste(RData, 'p.range.RData', sep = ''))
   save(stdev, file = paste(RData, 'stdev.RData', sep = ''))
+  save(rho, file = paste(RData, 'rho.RData', sep = ''))
   
   if (write == TRUE) {
-    writeLines(fixed.effects, paste(directory, 'fixed_effects', l, '_k', k, '.txt', sep = ''))
-    writeLines(hyperparameters, paste(directory, 'hyperparameters', l, '_k', k, '.txt', sep = ''))
-    capture.output(summary(mod.mode), file = paste(directory, 'summary', l, '_k', k, '.txt', sep = ''))}
-  
+    writeLines(fixed.effects, paste(directory, 'fixed.effects.', l, '.k.', k, '.txt', sep = ''))
+    writeLines(hyperparameters, paste(directory, 'hyperparameters.', l, '.k.', k, '.txt', sep = ''))
+    capture.output(summary(mod.mode), file = paste(directory, 'summary.', l, '.k.', k, '.txt', sep = ''))}
+
   return(invisible(NULL))}
