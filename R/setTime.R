9364c49775ab780f1a656b0e13b620f04d1e7884
Shenan Grossberg <s.grossberg@btinternet.com>
2021-04-27 16:11:47 +0100

    modified setTime.R for same reason as before

--- R/setTime.R
+++ R/setTime.R
@@ -11,8 +11,8 @@ set.time <- function(gldr.time.start, data.time.start, origin.time, train.length
   cov.time.training.start <- gldr.time.start - as.numeric(difftime(data.time.start, origin.time, tz = "GMT", units = "secs")) 
   # converts gldr.time.start from being relative to origin.time to being relative to data.time.start
   cov.time.training.end <- cov.time.training.start + (train.length - 1)*train.interval
-  cov.time.training <- seq(from = cov.time.training.start, to = cov.time.training.end, by = train.interval)
+  cov.time.training <- seq(from = c(cov.time.training.start), to = c(cov.time.training.end), by = train.interval)
   cov.time.predicting.start <- cov.time.training.end + flight.interval
   cov.time.predicting.end <- cov.time.predicting.start + (nt-1)*pred.interval
-  cov.time.predicting <- seq(from = cov.time.predicting.start, to = cov.time.predicting.end, by = pred.interval)
+  cov.time.predicting <- seq(from = c(cov.time.predicting.start), to = c(cov.time.predicting.end), by = pred.interval)
   return(list(cov.time.training, cov.time.predicting))}
